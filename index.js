d8535908ab44f0c62b023f5e5587a71b11210603
jay311311 <jay311311@gmail.com>
2020-08-25 23:47:20 +0900

    비밀번호 암호화 by bcrypt (password encryption by bcrypt)

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended : true}))
 // application/json 이렇게 생긴 데이터를 가져와서 분석 할수 있게 함
 app.use(bodyParser.json());
 
-// mongodb+srv://jay311311:<password>@cluster0.49l1z.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority
+
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -21,6 +21,7 @@ mongoose.connect(config.mongoURI,{
 
 
 //회원가입을 위한 라우트
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -28,8 +29,11 @@ app.get('/', (req, res) => {
   //회원가입할 떄 필요한 정보들을 client에서 가져오면 
   //그것들을 데이터 베이스에 넣어준다
 
+
+ //(resister router) 
 app.post(`/register`, (req,res)=>{
   const user = new User(req.body);// body는 bodyParser을 이용해 데이터를 객체로 정리된 내용
+  
   
   user.save((err,userInfo)=>{
     if (err) return res.json({success: false, err})
